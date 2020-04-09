5fe82927219763401b11b640a0ec32e117dac002
Bruno Costa Ribeiro <brunocostaribeiro@gmail.com>
2020-04-09 14:23:45 -0300

    Atualização de usuário com PUT

--- src/main/java/br/com/in1/workshopmongo/services/UserService.java
+++ src/main/java/br/com/in1/workshopmongo/services/UserService.java
@@ -47,23 +47,17 @@ public class UserService {
 			 throw new DatabaseException(e.getMessage());
 		}
 	}
+	*/
 	
-	public User update(Long id, User obj) {
-		try {
-			User entity = repository.getOne(id); //Prepara o objeto monitorado para alterar e depois gravar no banco
+	public User update(User obj) {
+			User entity = repository.findById(obj.getId()).get(); //Prepara o objeto monitorado para alterar e depois gravar no banco
 			updateData(entity, obj);
 			return repository.save(entity);
-		}
-		catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException(id);
-		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());
-		
 	}
-*/
+
 }
